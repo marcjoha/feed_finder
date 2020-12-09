e189137912a020ab8f704182b37c0011c9cccf43
Martin Jablecnik <martin.jablecnik@email.cz>
2020-12-09 19:50:30 +0100

    Add some optional arguments for faster finding

--- lib/feed_finder.dart
+++ lib/feed_finder.dart
@@ -8,7 +8,7 @@ import 'package:webfeed/webfeed.dart';
 /// elements in <head> and as unstructured links in the <body>.
 class FeedFinder {
   /// Returns feeds found on `url`
-  static Future<List<String>> scrape(String url) async {
+  static Future<List<String>> scrape(String url, { parseFromHead = true, parseFromBody = true, verify = true }) async {
     var results = <String>[];
     var candidates = <String>[];
 
@@ -26,31 +26,35 @@ class FeedFinder {
     var base = uri.scheme + '://' + uri.host;
 
     // Look for feed candidates in head
-    for (var link in document.querySelectorAll("link[rel='alternate']")) {
-      var type = link.attributes['type'];
-      if (type != null) {
-        if (type.contains('rss') || type.contains('xml')) {
-          var href = link.attributes['href'];
-          if (href != null) {
-            // Add base to relative URLs
-            href = href.startsWith('/') ? base + href : href;
-            candidates.add(href);
+    if (parseFromHead) {
+      for (var link in document.querySelectorAll("link[rel='alternate']")) {
+        var type = link.attributes['type'];
+        if (type != null) {
+          if (type.contains('rss') || type.contains('xml')) {
+            var href = link.attributes['href'];
+            if (href != null) {
+              // Add base to relative URLs
+              href = href.startsWith('/') ? base + href : href;
+              candidates.add(href);
+            }
           }
         }
       }
     }
 
     // Look for feed candidates in body
-    for (var a in document.querySelectorAll('a')) {
-      var href = a.attributes['href'];
-      if (href != null) {
-        if (href.contains('rss') ||
-            href.contains('xml') ||
-            href.contains('feed')) {
-          // Add base to relative URLs
-          href = href.startsWith('/') ? base + href : href;
-          href = href.endsWith('/') ? href.substring(0, href.length - 2) : href;
-          candidates.add(href);
+    if (parseFromBody) {
+      for (var a in document.querySelectorAll('a')) {
+        var href = a.attributes['href'];
+        if (href != null) {
+          if (href.contains('rss') ||
+              href.contains('xml') ||
+              href.contains('feed')) {
+            // Add base to relative URLs
+            href = href.startsWith('/') ? base + href : href;
+            href = href.endsWith('/') ? href.substring(0, href.length - 2) : href;
+            candidates.add(href);
+          }
         }
       }
     }
@@ -59,27 +63,31 @@ class FeedFinder {
     candidates = candidates.toSet().toList();
 
     // Verify candidates
-    for (var candidate in candidates) {
-      var response;
-      try {
-        response = await http.get(candidate);
-      } catch (e) {
-        continue;
-      }
-
-      // Check for both RSS and Atom feeds
-      try {
-        RssFeed.parse(response.body);
-      } catch (e) {
+    if (verify) {
+      for (var candidate in candidates) {
+        var response;
         try {
-          AtomFeed.parse(response.body);
+          response = await http.get(candidate);
         } catch (e) {
-          // Neither RSS nor Atom, go to next
           continue;
         }
-      }
 
-      results.add(candidate);
+        // Check for both RSS and Atom feeds
+        try {
+          RssFeed.parse(response.body);
+        } catch (e) {
+          try {
+            AtomFeed.parse(response.body);
+          } catch (e) {
+            // Neither RSS nor Atom, go to next
+            continue;
+          }
+        }
+
+        results.add(candidate);
+      }
+    } else {
+      results = candidates;
     }
 
     return results;
